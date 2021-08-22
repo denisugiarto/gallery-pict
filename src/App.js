b2ade16662ba423463cfdcfd3e3792e1cab41290
denzord <45736463+denzord@users.noreply.github.com>
2021-08-23 00:20:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import ImageGrid from "./component/ImageGrid";
 import { Title } from "./component/Title";
+import { UploadForm } from "./component/UploadForm";
+import Modal from "./component/Modal";
+import { useState } from "react";
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
       <Title />
+      <UploadForm />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
     </div>
   );
 }
